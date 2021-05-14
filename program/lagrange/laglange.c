46e00ef33921164ccd6505169e255b048e1f26de
Yuu-taremayu <taremayu.yuu@gmail.com>
2021-05-14 18:44:36 +0900

    implement lagrange interpolation (maybe)

--- program/lagrange/laglange.c
+++ program/lagrange/laglange.c
@@ -18,8 +18,8 @@ typedef const struct GF_info {
 } GF_info;
 
 /* lagrange interpolation */
-void lagrange(void);
-void base_poly(int dataNum, int i, unsigned int x, unsigned int dataX[]);
+unsigned int lagrange(int dataNum, unsigned int dataX[], unsigned int dataY[], GF_info GF);
+unsigned int base_poly(int dataNum, int i, unsigned int x, unsigned int dataX[], GF_info GF);
 
 /* arithmetic functions */
 unsigned int field_add(unsigned int x, unsigned int y);
@@ -32,6 +32,10 @@ int main(void)
 	GF_info GF = {
 		{0, 1, 2, 3, 4, 5, 6, 7},
 		{0, 1, 2, 4, 3, 6, 7, 5}};
+	int dataNum;
+	unsigned int dataX[] = {1, 4};
+	unsigned int dataY[] = {1, 2};
+	unsigned int L;
 	unsigned int testX = 4;
 	unsigned int testY = 5;
 	unsigned int testZ;
@@ -44,6 +48,10 @@ int main(void)
 	printf("%d * %d = %d\n", testX, testY, testZ);
 	testZ = field_div(testX, testY, GF);
 	printf("%d / %d = %d\n", testX, testY, testZ);
+
+	dataNum = sizeof(dataX) / sizeof(dataX[0]);
+	L = lagrange(dataNum, dataX, dataY, GF);
+	printf("L = %d\n", L);
 
 	return 0;
 }
@@ -131,13 +139,40 @@ unsigned int field_div(unsigned int x, unsigned int y, GF_info GF)
 	return EXIT_FAILURE;
 }
 
-void base_poly(int dataNum, int i, unsigned int x, unsigned int dataX[])
+unsigned int lagrange(int dataNum, unsigned int dataX[], unsigned int dataY[], GF_info GF)
 {
+	unsigned int x;
+	unsigned int l1;
+	unsigned int l2;
+	unsigned int l;
+	unsigned int L;
+	int i;
+
+	x = 0;
+	L = 0;
+
+	for (i = 0; i < dataNum; i++) {
+		l1 = base_poly(dataNum, i, x, dataX, GF);
+		l2 = base_poly(dataNum, i, dataX[i], dataX, GF);
+		l = field_div(l1, l2, GF);
+		L = field_add(L, field_mul(dataY[i], l, GF));
+	}
+
+	return L;
+}
+
+unsigned int base_poly(int dataNum, int i, unsigned int x, unsigned int dataX[], GF_info GF)
+{
+	unsigned int sub;
 	unsigned int l;
 	int j;
 
 	for (j = 0; j < dataNum; j++) {
 		if (j != i) {
+			sub = field_sub(x, dataX[j]);
+			l = field_mul(l, sub, GF);
 		}
 	}
+
+	return l;
 }
