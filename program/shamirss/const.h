73e2a8bd559baab14046ac7d33d05bc7477330e1
Yuu-taremayu <taremayu.yuu@gmail.com>
2021-07-15 14:16:16 +0900

    move define macro

---
+++ program/shamirss/const.h
@@ -0,0 +1,18 @@
+/* define message of usage */
+#define USAGE "Usage: %s [--mode=m] [FILE ...]\n"
+
+/* define extension of share file */
+#define EXT ".share"
+#define EXTLEN (int)strlen(EXT)
+
+/* Galois field size */
+/* 2^8 */
+/* also use modulo num */
+#define FIELD_SIZE 256
+
+/* Bit mask */
+/* use BIT_MASK for truncate upper bit */
+#define BIT_MASK 0xff
+
+/* for reading shares */
+#define BUFSIZE 2
