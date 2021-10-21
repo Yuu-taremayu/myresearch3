ef9d2af01303084890d4c5f22171dfe2fe013d42
Yuu-taremayu <taremayu.yuu@gmail.com>
2021-10-21 16:52:00 +0900

    fix comment

--- program/shamirss/ss_utils.h
+++ program/shamirss/ss_utils.h
@@ -1,16 +1,24 @@
-/* parameters of Secret Sharing */
+/*
+ * parameters of Secret Sharing
+ */
 typedef struct SS_param {
 	int k;
 	int n;
 } SS_param;
 
-/* operations of secret sharing */
+/*
+ * operations of secret sharing
+ */
 extern void split(char *path, int *GF_vector);
 extern void combine(char *path[], int shareNum, int *GF_vector);
 
-/* generating functions to prepare secret sharing */
+/*
+ * generating functions to prepare secret sharing
+ */
 extern void generate_server_id(int *serverId, int n);
 extern void generate_polynomial(int *poly, int secret, int k);
 
-/* create shares */
+/*
+ * create shares
+ */
 extern void create_shares(int *serverId, int *poly, int *shares, SS_param SS, int *GF_vector);
